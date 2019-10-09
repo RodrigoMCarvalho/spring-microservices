8f449932c662bd275b5728773e1ec8af512d408c
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-10-08 23:50:57 -0300

    criando microserviços

--- course/src/main/java/com/academy/course/service/CourseService.java
+++ course/src/main/java/com/academy/course/service/CourseService.java
@@ -2,6 +2,7 @@ package com.academy.course.service;
 
 import com.academy.course.model.Course;
 import com.academy.course.repository.CourseRepository;
+import lombok.extern.flogger.Flogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
